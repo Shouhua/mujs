b66c00dd0dac87abed036b884b559118374db431
pengshouhua <shouhua@outlook.com>
2023-04-19 19:53:20 +0800

    feat: 添加cmake练习

---
+++ note/demo/cmake_proj/t1/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+	printf("Hello world from t1\n");
+	return 0;
+}
---
+++ note/demo/cmake_proj/t2/src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+	printf("Hello world from t2\n");
+	return 0;
+}
